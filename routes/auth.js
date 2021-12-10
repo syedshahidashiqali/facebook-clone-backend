0c2e569dc37346bf68c2c8acaeeedbd05bf2658b
Syed Shahid <syedshahid1215@gmail.com>
2021-12-10 18:56:18 +0500

    add regiter route in auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,26 @@
 const router = require("express").Router();
+// import User model
+const User = require("../models/user");
 
-router.get("/", (req,res) => {
-    res.send("Its auth page")
+// REGISTER
+router.post("/register", async (req,res) => {
+    const newUser = await new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password,
+        profilePicture: req.body.profilePicture,
+        coverPicture: req.body.coverPicture,
+        followers: req.body.followers,
+        following: req.body.following,
+        isAdmin: req.body.isAdmin,
+    });
+
+    try{
+        const user = await newUser.save();
+        res.status(200).json(user)
+    } catch(err){
+        console.log(err)
+    }
 })
 
 module.exports = router;
