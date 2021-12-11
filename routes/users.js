fbb68ee490022c760e82e921191224886d94a733
Syed Shahid Ashiq Ali <syedshahid1215@gmail.com>
2021-12-11 19:11:09 +0500

    add unfollow user functionality in users route

--- routes/users.js
+++ routes/users.js
@@ -81,6 +81,28 @@ router.put("/:id/follow", async(req,res) => {
         res.status(403).json("You cannot follow yourself!");
     }
 });
+
 // unfollow a user
+router.put("/:id/unfollow", async(req, res) => {
+    // if not same users
+    if(req.body.userId !== req.params.id){
+
+        try{
+            const user = await User.findById(req.params.id);
+            const currentUser = await User.findById(req.body.userId);
+            if(user.followers.includes(req.body.userId)){
+                await user.updateOne({ $pull: { followers: req.body.userId } });
+                await currentUser.updateOne({ $pull: { followings: req.params.id } });
+                res.status(200).json("User has been unfollowed!");
+            } else{
+                res.status(403).json("You already unfollowed this user!");
+            }
+        } catch(err){
+            res.status(500).json(err);
+        }
+    } else {
+        res.status(403).json("You cannot unfollow yourself!");
+    }
+});
 
 module.exports = router;
