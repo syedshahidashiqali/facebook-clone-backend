163cb17390ee3d6aca48b56bb50e70116d46ab56
Syed Shahid Ashiq Ali <syedshahid1215@gmail.com>
2021-12-11 14:57:08 +0500

    add user updating functionality in users route

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,34 @@
 const router = require("express").Router();
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
 
-router.get("/", (req,res) => {
-    res.send("Its users page")
+// update user
+router.put("/:id", async(req,res) => {
+    if(req.body.userId === req.params.id || req.body.isAdmin){
+        if(req.body.password){
+            try{
+                const salt = await bcrypt.genSalt(10);
+                req.body.password = await bcrypt.hash(req.body.password, salt);
+            } catch(err){
+                res.status(500).json(err)
+            }
+        }
+        try{
+            // update(), updateMany(), findOneAndUpdate(), etc. 
+            // do not execute save() middleware. If you need save middleware and full validation, 
+            // first query for the document and then save() it.
+            const user = await User.findByIdAndUpdate(req.params.id, {$set: req.body});
+            res.status(200).json("Acount has been updated!");
+        } catch(err){
+            res.status(500).json(err)
+        }
+    } else{
+        res.status(403).json("You can update only your account!")
+    }
 })
+// delete user
+// get a user
+// follow a user
+// unfollow a user
 
 module.exports = router;
