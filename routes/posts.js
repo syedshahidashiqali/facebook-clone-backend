2d8a9b95d1499709a2194a52c321749622cf2de9
Syed Shahid Ashiq Ali <syedshahid1215@gmail.com>
2021-12-12 16:06:47 +0500

    add delete post functionality in posts route

--- routes/posts.js
+++ routes/posts.js
@@ -29,6 +29,21 @@ router.put("/:id", async(req, res) => {
 });
 
 // delete a post
+router.delete("/:id", async(req, res) => {
+
+    try {
+        const post = await Post.findById(req.params.id);
+        if(post.userId === req.body.userId) {
+            await post.deleteOne();
+            res.status(200).json("The post has been deleted!");
+        } else {
+            res.status(403).json("You can delete only your post!");
+        }
+    } catch(err){
+        res.status(500).json(err);
+    }
+});
+
 // like / dislike a post
 // get a post
 // get all posts i.e timeline posts
