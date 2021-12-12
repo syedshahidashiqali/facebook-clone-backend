0409b9bfac8fee4230e589af3f8964dd83222b9e
Syed Shahid Ashiq Ali <syedshahid1215@gmail.com>
2021-12-12 18:19:20 +0500

    add functionality for getting all timeline posts

--- routes/posts.js
+++ routes/posts.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const Post = require("../models/post");
+const User = require("../models/user");
 
 // create a post
 router.post("/", async(req, res) => {
@@ -71,5 +72,19 @@ router.get("/:id", async(req, res) => {
 });
 
 // get all posts i.e timeline posts
+router.get("/timeline/all", async(req, res) => {
+    try{
+        const currentUser = await User.findById(req.body.userId);
+        const userPosts = await Post.find({ userId: currentUser._id });
+        const friendPosts = await Promise.all(
+            currentUser.followings.map((friendId) => {
+                return Post.find({ userId: friendId });
+            })
+        );
+        res.status(200).json(userPosts.concat(...friendPosts));
+    } catch(err){
+        res.status(500).json(err);
+    }
+});
 
 module.exports = router;
